bff9dcbcc3b015030d266fa5106e72ecdc950fcc
unknown <videodesign2014@gmail.com>
2022-03-10 22:35:29 +0200

    task2j

--- src/elements/Dropdown/Cartdropdown.js
+++ src/elements/Dropdown/Cartdropdown.js
@@ -2,23 +2,73 @@ import React, { Component } from "react";
 //import cart from "../images/cart.png";
 //import CartContext from "../context/CartContext.js";
 import "../Dropdown/cartdropdown.css";
+import formatCurrency from "format-currency";
 
 export default class CartDropdown extends Component {
   render() {
-    //console.log(this.props.cartRange[0].brand);
+    let opts = { format: "%s%v", symbol: "$" };
+    console.log(this.props);
     return (
       <div className="dropdown_main">
-       Cart range here: {this.props.cartRange[0].brand}
-        <div>{this.props.cartRange.map((item) => {
-          <div>
-            <p>{item.brand}</p>
-          </div>;
-          <div>{item.category}</div>;
-        })}</div>
+        <p className="mybag">My bag:</p>{" "}
+        <div className="dropdown_items">
+          {this.props.cartRange.map((item) => (
+            <div key={item.id}>
+              <div className="drrow">
+                <div className="dritemleft">
+                  <div>
+                    <p className="drbrand">{item.name}</p>
+                  </div>
+                  <div>
+                    <p className="dramount">
+                      {formatCurrency(`${item.prices[0].amount}`, opts)}
+                    </p>
+                  </div>
+                  <div>
+                    <label className="radioLabel">
+                      <div
+                        style={{
+                          width: "24px",
+                          height: "24px",
+                          border: "1px solid rgb(29, 31, 34)",
+                          backgroundColor: item.value,
+                        }}
+                      />
+                    </label>
+                  </div>
+                  {/*<p>{item.attributes[1].items.displayValue}</p>*/}
+                </div>
+                <div className="dritemright">
+                  <div className="rightrow">
+                    <div className="drcounter">
+                      <div>
+                        <button className="drcounterbutt">+</button>
+                      </div>
+                      <div>
+                        <p>1</p>
+                      </div>
+                      <div>
+                        <button className="drcounterbutt">-</button>
+                      </div>
+                    </div>
+                    <img className="drcartimage" src={item.gallery} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="bottomblock">
+          <div className="total">Total:</div>
+          <div className="drbuttons">
+            <button className="drbuttontotal">Total</button>
+            <button className="drbuttoncheckout">Chekout</button>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 //<h1>Item is {this.props.cartRange.length}</h1>
-// 
+// {this.props.cartRange[0].brand}
